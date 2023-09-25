18a83787adb98b8752d82df3e03e32c301e3bab8
David Menache <dave@doap.com>
2023-09-24 22:16:32 -0600

    Update fetch_discover_listings.sh

--- fetch_discover_listings.sh
+++ fetch_discover_listings.sh
@@ -3,12 +3,12 @@
 #get page 1
     url="https://discoversjds.com/property-search/?keyword&location=any&status=any&type=any&bedrooms=any&bathrooms=any&min-price=any&max-price=any&min-area&max-area"
     filename="page1.html"
-    curl -o "current/$filename" "$url"
+    curl -o "discover/$filename" "$url"
 
 # Fetch pages 2 to 20
 for i in {2..20}
 do
     url="https://discoversjds.com/property-search/page/${i}/?keyword&location=any&status=any&type=any&bedrooms=any&bathrooms=any&min-price=any&max-price=any&min-area&max-area"
     filename="page${i}.html"
-    curl -o "current/$filename" "$url"
+    curl -o "discover/$filename" "$url"
 done
